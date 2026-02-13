59281418fbf8f198c7cf3daa714425540314c794
Mir Moniruzzaman <mirmuniruzzaman303@gmail.com>
2026-02-13 09:40:43 +0600

    some changed in home piechart

--- app/(admin)/home/OrderPieChart.tsx
+++ app/(admin)/home/OrderPieChart.tsx
@@ -1,8 +1,8 @@
-"use client"
+"use client";
 
-import { TrendingUp } from "lucide-react"
-import { Pie, PieChart, Sector } from "recharts"
-import { type PieSectorDataItem } from "recharts/types/polar/Pie"
+import { TrendingUp } from "lucide-react";
+import { Pie, PieChart, Sector } from "recharts";
+import { type PieSectorDataItem } from "recharts/types/polar/Pie";
 
 import {
   Card,
@@ -11,19 +11,19 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "../../components/ui/chart/card"
+} from "../../components/ui/chart/card";
 import {
   ChartContainer,
   ChartTooltip,
   ChartTooltipContent,
   type ChartConfig,
-} from "../../components/ui/chart/chart"
+} from "../../components/ui/chart/chart";
 
 const chartData = [
   { category: "electronics", sales: 450, fill: "var(--color-electronics)" },
   { category: "fashion", sales: 380, fill: "var(--color-fashion)" },
   { category: "accessories", sales: 240, fill: "var(--color-accessories)" },
-]
+];
 
 const chartConfig = {
   sales: {
@@ -35,33 +35,38 @@ const chartConfig = {
   },
   fashion: {
     label: "Fashion",
-    color: "#34D399", 
+    color: "#34D399",
   },
   accessories: {
     label: "Accessories",
     color: "#FBBF24",
   },
-} satisfies ChartConfig
+} satisfies ChartConfig;
 
 export function OrderPieChart() {
   return (
-    <Card className="flex flex-col border-none shadow-none bg-white dark:bg-[#2d275f]  px-5 py-5">
+    <Card className="flex flex-col border-none  bg-white dark:bg-[#2d275f]  px-5 py-5">
       <CardHeader className="items-center pb-0 px-0">
-        <CardTitle className="text-lg font-bold">Category Distribution</CardTitle>
-        <CardDescription>Sales Share - 2025</CardDescription>
+        <CardTitle className="text-lg font-bold">
+          Category Distribution
+        </CardTitle>
+        <CardDescription>
+          <div className="flex flex-col gap-y-4">
+            <ChartLabel text="electronies" />
+            <ChartLabel text="fashion" bgColor="bg-[#34D399]" />
+            <ChartLabel text="accessories" bgColor="bg-[#FBBF24]" />
+          </div>
+        </CardDescription>
       </CardHeader>
       <CardContent className="flex-1 pb-0 px-0">
-        <ChartContainer
-          config={chartConfig}
-          className="mx-auto aspect-square "
-        >
+        <ChartContainer config={chartConfig} className="mx-auto aspect-square ">
           <PieChart>
             <ChartTooltip
               cursor={false}
               content={
-                <ChartTooltipContent 
-                   hideLabel 
-                   className="bg-white text-black border-gray-200 shadow-md"
+                <ChartTooltipContent
+                  hideLabel
+                  className="bg-white text-black dark:text-white border-gray-200 shadow-md"
                 />
               }
             />
@@ -71,7 +76,7 @@ export function OrderPieChart() {
               nameKey="category"
               innerRadius={0}
               strokeWidth={8}
-              stroke="transparent" 
+              stroke="transparent"
               activeIndex={0}
               activeShape={({
                 outerRadius = 0,
@@ -85,12 +90,30 @@ export function OrderPieChart() {
       </CardContent>
       <CardFooter className="flex-col gap-2 text-sm px-0 pt-4">
         <div className="flex items-center gap-2 leading-none font-medium">
-          Top performer: Electronics <TrendingUp className="h-4 w-4 text-emerald-500" />
+          Top performer: Electronics{" "}
+          <TrendingUp className="h-4 w-4 text-emerald-500" />
         </div>
         <div className="text-center">
           Showing sales distribution across top 3 categories
         </div>
       </CardFooter>
     </Card>
-  )
+  );
 }
+
+interface labelProps {
+  text: string;
+  bgColor?: string;
+}
+const ChartLabel: React.FC<labelProps> = ({
+  text,
+  bgColor = "bg-[#38BDF8]",
+}) => {
+  return (
+    <>
+      <div className="flex gap-x-2 items-center text-gray-600 dark:text-white capitalize">
+        <span>{text} </span> <div className={`w-8 h-3 ${bgColor}`}></div>
+      </div>
+    </>
+  );
+};
